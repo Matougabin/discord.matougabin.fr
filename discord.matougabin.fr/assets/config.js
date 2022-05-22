da1ecbb1b4b218c983ac0dcb2a2d1c6e05e65502
Matougabin#0001 <matougabin@gmail.com>
2022-05-22 10:02:48 +0200

    Add files via upload

---
+++ discord.matougabin.fr/assets/config.js
@@ -0,0 +1,27 @@
+const config = {
+    
+    USERNAME: "Matougabin#0001",
+    PICTURE: "a525bccf3d7d04458e1d04bfa9d54230",
+
+    DESCRIPTION: "Video game players like @Fortnite, @Roblox, @GTA. Youtuber Mad City (Roblox) French. Discord Bots developer like @Stan Bot.",
+
+    BADGES: [
+        { id: "HypeSquad Balance", image: "assets/img/hypesquad_balance.svg" },
+        { id: "Nitro Sub", image: "assets/img/nitro_sub.svg" },
+        { id: "Nitro Boosting 3 Months Badge", image: "assets/img/boost_3_months_badge.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "team-matougabin", image: "assets/img/team_matougabin.svg", link: "https://discord.gg/mM9h9H65z6", name: "Team Matougabin" },
+        { id: "stan-bot", image: "assets/img/stan_bot.svg", link: "https://discord.gg/ERvhxtmUvD", name: "Support - Stan Bot" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Matougabin" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/Matougabin" },
+        { class_name: "fab fa-discord", link: "https://discord.gg/mM9h9H65z6" },
+        { class_name: "fab fa-instagram", link: "https://www.instagram.com/matougabin/" },
+        { class_name: "fab fa-youtube", link: "https://www.youtube.com/c/Matougabin" },
+    ],
+
+}
